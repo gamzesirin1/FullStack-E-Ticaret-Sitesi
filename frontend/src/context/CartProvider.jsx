3f484e8c743d26443e0c59be4c7fa68b42b83245
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-11-01 11:44:18 +0300

    cart sepet işlemleri  edited.

--- frontend/src/context/CartProvider.jsx
+++ frontend/src/context/CartProvider.jsx
@@ -1,20 +1,14 @@
 import { createContext, useEffect, useState } from 'react'
 
 import PropTypes from 'prop-types'
-
-const CartContext = createContext()
-
+export const CartContext = createContext()
 const CartProvider = ({ children }) => {
 	const [cartItems, setCartItems] = useState(
 		localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 	)
-
 	useEffect(() => {
 		localStorage.setItem('cartItems', JSON.stringify(cartItems))
 	}, [cartItems])
-
-	console.log('cart items 1:', cartItems)
-
 	const addToCart = (cartItem) => {
 		// setCartItems([...cartItems, cartItem]); 1. yol
 		setCartItems((prevCart) => [
@@ -25,21 +19,27 @@ const CartProvider = ({ children }) => {
 			}
 		])
 	}
-	const removeFromCard = (itemId) => {
+	const removeFromCart = (itemId) => {
 		const filteredCartItems = cartItems.filter((cartItem) => {
 			return cartItem._id !== itemId
 		})
-
 		setCartItems(filteredCartItems)
 	}
 
-	return <CartContext.Provider value={{ addToCart, removeFromCard, cartItems }}>{children}</CartContext.Provider>
+	return (
+		<CartContext.Provider
+			value={{
+				cartItems,
+				setCartItems,
+				addToCart,
+				removeFromCart
+			}}
+		>
+			{children}
+		</CartContext.Provider>
+	)
 }
-
 export default CartProvider
-
-export { CartContext }
-
 CartProvider.propTypes = {
 	children: PropTypes.node
 }
